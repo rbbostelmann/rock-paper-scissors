f3e6fb572d760d113e46f38d539346997c03d919
rbbostelmann <rodrigobostelmann@gmail.com>
2022-02-16 18:47:15 -0300

    js updated but still not working as intended

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+The computer is very salty, beware.
--- script.js
+++ script.js
@@ -1,43 +1,78 @@
-//Objects with possible outcomes
+//Some global variables we'll need
+let playerWins = 0;
+let playerLosses = 0;
+let numberTies = 0;
+let invalidInputs = 0;
+const maxMatches = 5;
+
+//Possible outcomes
 const outcomes = {
     Rock: { weakTo: "Paper", strongAgainst: "Scissors" },
     Paper: { weakTo: "Scissors", strongAgainst: "Rock" },
-    Scissors: { weakTo: "Rock", strongAgainst: "Paper" }
+    Scissors: { weakTo: "Rock", strongAgainst: "Paper" },
+    Messages: { win: "You win!", loss: "The computer wins!", tie: "It's a tie!", invalid: "Invalid input!" }
 };
 
 //Array with possible choices
 const choices = ["Rock", "Paper", "Scissors"];
 
 //Random choice for the computer
-const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+let computerSelection = choices[Math.floor(Math.random() * choices.length)];
 
 //Get user input and applies uppercase to the word
-const userInput = prompt("Pick rock, paper or scissors: ");
-const playerSelection = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+let userInput = prompt("Pick rock, paper or scissors: ");
+let playerSelection = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+///ALL ABOVE WORKING FINE
+
 
 //Function that playes one match and gives the result.
 function playMatch(playerSelection, computerSelection) {
     if (playerSelection !== "Rock" && playerSelection !== "Paper" &&
         playerSelection !== "Scissors") {
-        return "Invalid input. Choose ONLY from ROCK, PAPER, or SCISSORS!"
+        invalidInputs++;
+        return outcomes.Messages.invalid;
     } else if (outcomes[playerSelection].weakTo === computerSelection) {
-        return "The computer wins!"
+        playerLosses++;
+        gameOver();
+        return outcomes.Messages.loss
     } else if (outcomes[playerSelection].strongAgainst === computerSelection) {
-        return "You win, asshole."
+        playerWins++;
+        gameOver();
+        return outcomes.Messages.win
     } else {
-        return "It's a tie!"
+        numberTies++;
+        return outcomes.Messages.tie
     }
 }
 
-//debug line
-console.log(playerSelection)
-console.log(computerSelection)
+function playGame() {
+    for (let i = 0; i < maxMatches; i++) {
+        computerSelection = choices[Math.floor(Math.random() * choices.length)];
+        let userInput = prompt("Pick rock, paper or scissors: ");
+        let playerSelection = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+        playMatch(playerSelection, computerSelection);
+    }
 
-console.log(playMatch(playerSelection, computerSelection));
-
-//playMatch(playerSelection, computerSelection);
-
-//Continue from here tomorrow
-function game() {
-    playMatch()
 }
+
+//Not working as intended; instead of getting me/pc to 5 wins it plays only five games total
+function gameOver() {
+
+    if (playerWins >= 5) {
+        return "You completely destroyed the machine."
+    } else if (playerLosses >= 5) {
+        return "The computer absolutely dominated you.";
+    }
+}
+
+//Only triggering on the first set of inputs (debug)
+console.log(`You played: ${playerSelection}`)
+console.log(`The computer played: ${computerSelection}`)
+
+playGame();
+
+//Working just fine
+console.log(`You won ${playerWins} time(s).
+The computer won ${playerLosses} time(s). 
+There were ${numberTies} tie(s).
+There were ${invalidInputs} invalid input(s).`)
